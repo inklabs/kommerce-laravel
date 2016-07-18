2f7a5edf5de41856abe3e5b2dc8fbdc0efac1950
Jason Raimondi <jason@raimondi.us>
2016-07-17 22:11:09 -0700

    Update layout files.

--- resources/views/layouts/content/topbar.blade.php
+++ resources/views/layouts/content/topbar.blade.php
@@ -14,8 +14,8 @@
         </div>
         <div class="top-bar-right">
             <ul class="menu">
-                <li><input type="search" placeholder="Search"></li>
-                <li><button type="button" class="button">Search</button></li>
+                {{--<li><input type="search" placeholder="Search"></li>--}}
+                {{--<li><button type="button" class="button">Search</button></li>--}}
             </ul>
         </div>
     </div>
