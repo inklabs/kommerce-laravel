0fef63bdbeec7f98e6c649f23b0cf929090b16b5
Jason Raimondi <jason@raimondi.us>
2016-07-22 23:52:07 -0700

    Clean up script imports.

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -6,45 +6,44 @@
     <title>Larakommerce</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
 
     <!-- Place favicon.ico in the root directory -->
     <link rel="icon" href="/favicon.ico">
 
-    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
+
 </head>
 <body>
 
-<header class="lk-topbar">
-    <div class="top-bar">
-        <div class="top-bar-title">
-        <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
-          <button class="menu-icon dark" type="button" data-toggle></button>
-        </span>
-            <strong>Larakommerce</strong>
-        </div>
-        <div id="responsive-menu">
-            <div class="top-bar-left">
-                <ul class="menu">
-                    <li><a href="#">Two</a></li>
-                    <li><a href="#">Three</a></li>
-                </ul>
+    <header class="lk-topbar">
+        <div class="top-bar">
+            <div class="top-bar-title">
+            <span data-responsive-toggle="responsive-menu" data-hide-for="medium">
+              <button class="menu-icon dark" type="button" data-toggle></button>
+            </span>
+                <strong>Larakommerce</strong>
             </div>
-            <div class="top-bar-right">
-                <ul class="menu">
-                    {{--<li><input type="search" placeholder="Search"></li>--}}
-                    {{--<li><button type="button" class="button">Search</button></li>--}}
-                </ul>
+            <div id="responsive-menu">
+                <div class="top-bar-left">
+                    <ul class="menu">
+                        <li><a href="#">Two</a></li>
+                        <li><a href="#">Three</a></li>
+                    </ul>
+                </div>
+                <div class="top-bar-right">
+                    <ul class="menu">
+                        {{--<li><input type="search" placeholder="Search"></li>--}}
+                        {{--<li><button type="button" class="button">Search</button></li>--}}
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
+    </header>
 
-</header>
+    <div id="lk-app"></div>
 
-<div id="lk-app"></div>
-
-<script src="{{ asset('assets/common.js') }}"></script>
-<script src="{{ asset('assets/app.js') }}"></script>
-<script src="{{ asset('assets/package.js') }}"></script>
+<script src="{{ asset('assets/main.js') }}"></script>
 
 </body>
 </html>
