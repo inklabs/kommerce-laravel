c520bdbc115c517d9965d6cfe353fac847c15314
Jason Raimondi <jason@raimondi.us>
2016-07-22 23:53:18 -0700

    Add breadcrumbs view and more edit fields.

---
+++ resources/assets/js/components/Breadcrumbs.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav aria-label="You are here:" role="navigation">
+    <ul class="breadcrumbs">
+      <li v-for="item in list"><span class="active" v-if="isLast($index)">{{item.label}}</span>
+        <a v-link="{ name: item.link }" v-else>{{ item.label }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    props: {
+      list: {
+        type: Array,
+        required: true,
+        default: () => []
+      },
+      separator: {
+        type: String
+      }
+    },
+    ready () {
+      if (this.separator) {
+        this.$el.style.setProperty('--separator', `"${this.separator}"`)
+      }
+    },
+    methods: {
+      isLast (index) {
+        return index === this.list.length - 1
+      }
+    }
+  }
+</script>
