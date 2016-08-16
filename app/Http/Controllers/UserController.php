edef048265996fffe2ea45c6396bc3a403e45127
Jamie Isaacs <pdt256@gmail.com>
2016-08-15 21:18:38 -0700

    Adding change password page.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,10 @@ use Illuminate\Http\Request;
 use inklabs\kommerce\Action\Order\GetOrderQuery;
 use inklabs\kommerce\Action\Order\Query\GetOrderRequest;
 use inklabs\kommerce\Action\Order\Query\GetOrderResponse;
+use inklabs\kommerce\Action\User\ChangePasswordCommand;
 use inklabs\kommerce\Exception\EntityNotFoundException;
+use inklabs\kommerce\Exception\KommerceException;
+use inklabs\kommerce\Exception\UserPasswordValidationException;
 use inklabs\kommerce\tests\Helper\Entity\DummyData;
 
 class UserController extends Controller
@@ -25,6 +28,45 @@ class UserController extends Controller
                 'orders' => $orders,
             ]
         );
+    }
+
+    public function getChangePassword()
+    {
+        $dummyData = new DummyData();
+        $user = $this->getDTOBuilderFactory()->getUserDTOBuilder($dummyData->getUser())->build();
+
+        $this->displayTemplate(
+            'user/change_password.twig'
+        );
+    }
+
+    public function postChangePassword(Request $request)
+    {
+        $passwordNew = $request->input('passwordNew');
+        $passwordCheck = $request->input('passwordCheck');
+
+        if ($passwordNew !== $passwordCheck) {
+            $this->flashError('Please check that your passwords match and try again.');
+            return redirect('/user/change-password');
+        }
+
+        $userId = 'xxx';
+
+        try {
+            $this->dispatch(new ChangePasswordCommand(
+                $userId,
+                $passwordNew
+            ));
+        } catch (UserPasswordValidationException $e) {
+            $this->flashError($e->getMessage());
+            return redirect('/user/change-password');
+        } catch (KommerceException $e) {
+            $this->flashGenericWarning();
+            return redirect('/user/account');
+        }
+
+        // TODO: Get correct logged-in user and redirect after successful password change
+        dd([$passwordNew, $passwordCheck]);
     }
 
     /**
