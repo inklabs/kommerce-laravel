ec316dcbcdcb76ad943caac8f421dc71820a95a3
Jamie Isaacs <pdt256@gmail.com>
2016-09-06 22:59:28 -0700

    Adding admin order actions.

---
+++ app/Http/Controllers/Admin/Order/AddShipmentController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Admin\Order;
+
+use App\Http\Controllers\Controller;
+
+class AddShipmentController extends Controller
+{
+    public function index($orderId)
+    {
+        echo 'TODO';
+    }
+}
---
+++ app/Http/Controllers/Admin/Order/SetOrderStatusController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Admin\Order;
+
+use App\Http\Controllers\Controller;
+
+class SetOrderStatusController extends Controller
+{
+    public function index()
+    {
+        echo 'TODO';
+    }
+}
