c3789b291c0783fa8939ab089d978e21ab596075
Jamie Isaacs <pdt256@gmail.com>
2016-09-09 18:06:08 -0700

    Adding add shipment tracking code page.

--- app/Http/Controllers/Admin/Order/AddShipmentController.php
+++ app/Http/Controllers/Admin/Order/AddShipmentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use inklabs\kommerce\Action\Order\GetOrderQuery;
 use inklabs\kommerce\Action\Order\Query\GetOrderRequest;
 use inklabs\kommerce\Action\Order\Query\GetOrderResponse;
+use inklabs\kommerce\Entity\ShipmentCarrierType;
 use inklabs\kommerce\Exception\EntityNotFoundException;
 
 class AddShipmentController extends Controller
@@ -29,6 +30,40 @@ class AddShipmentController extends Controller
     }
 
     public function post(Request $request)
+    {
+        $action = $request->input('action');
+
+        if ($action === 'create-with-tracking-code') {
+            return $this->createWithTrackingCode($request);
+        } elseif ($action === 'create-shipping-label') {
+            return $this->createShippingLabel($request);
+        }
+    }
+
+    private function createWithTrackingCode(Request $request)
+    {
+        $orderId = $request->input('orderId');
+        $orderItemQty = $request->input('orderItemQty');
+
+        try {
+            $request = new GetOrderRequest($orderId);
+            $response = new GetOrderResponse();
+            $this->dispatchQuery(new GetOrderQuery($request, $response));
+        } catch (EntityNotFoundException $e) {
+            return abort(404);
+        }
+
+        return $this->renderTemplate(
+            'admin/order/add-shipment-tracking.twig',
+            [
+                'order' => $response->getOrderDTOWithAllData(),
+                'orderItemQty' => $orderItemQty,
+                'shipmentCarrierTypes' => ShipmentCarrierType::getNameMap(),
+            ]
+        );
+    }
+
+    private function createShippingLabel(Request $request)
     {
         echo 'TODO:';
         dd($request->input());
