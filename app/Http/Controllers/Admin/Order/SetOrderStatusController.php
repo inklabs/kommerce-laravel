f74302173da33621de45b33448f640175ad3227c
Jamie Isaacs <pdt256@gmail.com>
2016-09-13 09:50:59 -0700

    Adding set order status.

--- app/Http/Controllers/Admin/Order/SetOrderStatusController.php
+++ app/Http/Controllers/Admin/Order/SetOrderStatusController.php
@@ -2,11 +2,28 @@
 namespace App\Http\Controllers\Admin\Order;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use inklabs\kommerce\Action\Order\SetOrderStatusCommand;
+use inklabs\kommerce\Exception\EntityValidatorException;
 
 class SetOrderStatusController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        echo 'TODO';
+        $orderId = $request->input('orderId');
+        $orderStatus = (int) $request->input('orderStatus');
+
+        try {
+            $this->dispatch(new SetOrderStatusCommand(
+                $orderId,
+                $orderStatus
+            ));
+
+            $this->flashSuccess('Changed Order Status.');
+        } catch (EntityValidatorException $e) {
+            $this->flashGenericWarning();
+        }
+
+        return redirect()->route('admin.order.view', ['orderId' => $orderId]);
     }
 }
