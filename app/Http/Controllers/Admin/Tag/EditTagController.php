604261ad81848d36fcd525640da30dda847f3d29
Jamie Isaacs <pdt256@gmail.com>
2016-12-19 13:18:41 -0800

    Closes https://github.com/inklabs/kommerce-core/issues/66.

--- app/Http/Controllers/Admin/Tag/EditTagController.php
+++ app/Http/Controllers/Admin/Tag/EditTagController.php
@@ -2,7 +2,11 @@
 namespace App\Http\Controllers\Admin\Tag;
 
 use App\Http\Controllers\Controller;
+use App\Lib\Arr;
 use Illuminate\Http\Request;
+use inklabs\kommerce\Action\Tag\UpdateTagCommand;
+use inklabs\kommerce\EntityDTO\TagDTO;
+use inklabs\kommerce\Exception\EntityValidatorException;
 
 class EditTagController extends Controller
 {
@@ -16,5 +20,39 @@ class EditTagController extends Controller
                 'tag' => $tag,
             ]
         );
+    }
+
+    public function post(Request $request)
+    {
+        $tagId = $request->input('tagId');
+        $tag = $this->getTag($tagId);
+
+        $this->updateTagDTOFromPost($tag, $request->input('tag'));
+
+        try {
+            $this->dispatch(new UpdateTagCommand($tag));
+
+            $this->flashSuccess('Tag has been saved.');
+            return redirect()->route(
+                'admin.tag.edit',
+                [
+                    'tagId' => $tagId,
+                ]
+            );
+        } catch (EntityValidatorException $e) {
+            $this->flashError('Unable to save tag!');
+            $this->flashFormErrors($e->getErrors());
+        }
+    }
+
+    private function updateTagDTOFromPost(TagDTO & $tagDTO, array $tagValues)
+    {
+        $tagDTO->name = trim(Arr::get($tagValues, 'name'));
+        $tagDTO->code = trim(Arr::get($tagValues, 'code'));
+        $tagDTO->description = trim(Arr::get($tagValues, 'description'));
+        $tagDTO->isActive = (bool) Arr::get($tagValues, 'isActive', false);
+        $tagDTO->isVisible = (bool) Arr::get($tagValues, 'isVisible', false);
+        $tagDTO->areAttachmentsEnabled = (bool) Arr::get($tagValues, 'areAttachmentsEnabled', false);
+        $tagDTO->sortOrder = Arr::get($tagValues, 'sortOrder', 0);
     }
 }
