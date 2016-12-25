806b991a9888509e1816eb5651187246d8b03031
Jamie Isaacs <pdt256@gmail.com>
2016-12-25 13:57:28 -0800

    create tax rate commands. https://github.com/inklabs/kommerce-core/issues/75

--- app/Http/Controllers/Admin/Settings/EditTaxRateController.php
+++ app/Http/Controllers/Admin/Settings/EditTaxRateController.php
@@ -2,8 +2,14 @@
 namespace App\Http\Controllers\Admin\Settings;
 
 use App\Http\Controllers\Controller;
+use App\Lib\Arr;
+use Exception;
 use Illuminate\Http\Request;
+use inklabs\kommerce\Action\TaxRate\CreateStateTaxRateCommand;
+use inklabs\kommerce\Action\TaxRate\CreateZip5RangeTaxRateCommand;
+use inklabs\kommerce\Action\TaxRate\CreateZip5TaxRateCommand;
 use inklabs\kommerce\Action\TaxRate\DeleteTaxRateCommand;
+use inklabs\kommerce\Exception\EntityValidatorException;
 use inklabs\kommerce\Exception\KommerceException;
 
 class EditTaxRateController extends Controller
@@ -12,22 +18,53 @@ class EditTaxRateController extends Controller
     {
         $type = $request->input('type');
 
-        $rate = (float) $request->input('rate');
-        $applyToShipping = (bool) $request->input('applyToShipping', false);
+        $taxRate = $request->input('taxRate');
+        $rate = (float) Arr::get($taxRate, 'rate');
+        $applyToShipping = (bool) Arr::get($taxRate, 'applyToShipping', false);
 
-        if ($type === 'zip5TaxRate') {
-            // TODO CreateZip5TaxRateCommand
-            $zip5 = $request->input('zip5');
-        } elseif ($type = 'zip5RangeTaxRate') {
-            // TODO CreateZip5RangeTaxRateCommand
-            $zip5From = $request->input('zip5From');
-            $zip5To = $request->input('zip5To');
-        } elseif ($type = 'stateTaxRate') {
-            // TODO CreateStateTaxRateCommand
-            $state = $request->input('state');
+        try {
+            if ($type === 'zip5TaxRate') {
+                $zip5 = Arr::get($taxRate, 'zip5');
+
+                $this->dispatch(new CreateZip5TaxRateCommand(
+                    $zip5,
+                    $rate,
+                    $applyToShipping
+                ));
+
+            } elseif ($type === 'zip5RangeTaxRate') {
+                // TODO CreateZip5RangeTaxRateCommand
+                $zip5From = Arr::get($taxRate, 'zip5From');
+                $zip5To = Arr::get($taxRate, 'zip5To');
+
+                $this->dispatch(new CreateZip5RangeTaxRateCommand(
+                    $zip5From,
+                    $zip5To,
+                    $rate,
+                    $applyToShipping
+                ));
+
+            } elseif ($type === 'stateTaxRate') {
+                $state = Arr::get($taxRate, 'state');
+
+                $this->dispatch(new CreateStateTaxRateCommand(
+                    $state,
+                    $rate,
+                    $applyToShipping
+                ));
+            } else {
+                throw new Exception();
+            }
+            $this->flashSuccess('Tax rate has been created.');
+
+        } catch (EntityValidatorException $e) {
+            $this->flashError('Unable to create tax rate!');
+            $this->flashFormErrors($e->getErrors());
+        } catch (Exception $e) {
+            $this->flashError('Unable to create tax rate!');
         }
-        echo '<pre>';
-        print_r($request->input());
+
+        return redirect()->route('admin.settings.sales-tax');
     }
 
     public function postEdit(Request $request)
