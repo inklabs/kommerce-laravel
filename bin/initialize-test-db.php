3779dab6dafe0f8a65c373a4a6bd4878b202c0f6
Jamie Isaacs <pdt256@gmail.com>
2017-01-18 20:36:13 -0800

    Initializing test database with various shirts.

--- bin/initialize-test-db.php
+++ bin/initialize-test-db.php
@@ -1,11 +1,23 @@
 <?php
-use Doctrine\ORM\Query\ResultSetMapping;
+use inklabs\kommerce\Entity\Attribute;
+use inklabs\kommerce\Entity\AttributeValue;
+use inklabs\kommerce\Entity\Image;
+use inklabs\kommerce\Entity\Product;
+use inklabs\kommerce\Entity\ProductAttribute;
+use inklabs\kommerce\Entity\Tag;
 use inklabs\kommerce\Entity\TaxRate;
 use inklabs\kommerce\Entity\User;
 use inklabs\kommerce\Entity\UserRole;
 use inklabs\kommerce\Entity\UserRoleType;
 
 require_once __DIR__  . '/../config/cli-config.php';
+
+unlink(__DIR__ . '/../storage/db.sqlite');
+$classes = $entityManager->getMetaDataFactory()->getAllMetaData();
+$tool = new Doctrine\ORM\Tools\SchemaTool($entityManager);
+$tool->dropSchema($classes);
+$tool->createSchema($classes);
+
 
 $adminRole = new UserRole(UserRoleType::admin());
 
@@ -34,24 +46,68 @@ $entities = [
     TaxRate::createState('CA', 8.5, true),
 ];
 
-$truncateEntities = [
-    'zk_Order',
-    'zk_TaxRate',
-    'zk_User',
-    'zk_UserRole',
-    'zk_user_userrole',
-];
+$attribute = new Attribute('Size', 1);
+$attributeValueLarge = new AttributeValue($attribute, 'Large', 1);
+$attributeValueMedium = new AttributeValue($attribute, 'Medium', 2);
+$attributeValueSmall = new AttributeValue($attribute, 'Small', 3);
 
-// TODO: Delete sqlite database file instead of truncating
-foreach ($truncateEntities as $class) {
-    $entityManager->createNativeQuery('DELETE FROM ' . $class, new ResultSetMapping())
-        ->execute();
-}
-
-//$entityManager->createNativeQuery('PRAGMA foreign_keys = ON', new ResultSetMapping())->execute();
+addShirts('Red Shirt',    'SHT-RED', 'http://i.imgur.com/mFpzjL2.jpg', 550, 600);
+addShirts('Blue Shirt',   'SHT-BLU', 'http://i.imgur.com/3c68pg3.jpg', 550, 600);
+addShirts('Gray Shirt',   'SHT-GRY', 'http://i.imgur.com/Wzc9lUc.jpg', 550, 600);
+addShirts('Green Shirt',  'SHT-GRN', 'http://i.imgur.com/6oKbsLw.jpg', 550, 600);
+addShirts('Orange Shirt', 'SHT-ONG', 'http://i.imgur.com/y7fSopr.jpg', 550, 600);
 
 foreach ($entities as $entity) {
     /** Doctrine\ORM\EntityManager $entityManager */
     $entityManager->persist($entity);
-    $entityManager->flush();
+}
+
+$entityManager->flush();
+
+
+function addShirts($name, $sku, $path, $width, $height)
+{
+    global $entities, $attribute, $attributeValueLarge, $attributeValueMedium, $attributeValueSmall;
+
+    $image = new Image();
+    $image->setPath($path);
+    $image->setWidth($width);
+    $image->setHeight($height);
+
+    $large = getProduct('Large ' . $name, $sku . '-LG', 1000, $image);
+    $medium = getProduct('Medium ' . $name, $sku . '-MD', 800, $image);
+    $small = getProduct('Small ' . $name, $sku . '-SM', 600, $image);
+
+    $attribute->addProductAttribute(new ProductAttribute($large, $attributeValueLarge));
+    $attribute->addProductAttribute(new ProductAttribute($medium, $attributeValueMedium));
+    $attribute->addProductAttribute(new ProductAttribute($small, $attributeValueSmall));
+
+    $tag = new Tag();
+    $tag->setName($name);
+    $tag->addImage($image);
+    $tag->addProduct($large);
+    $tag->addProduct($medium);
+    $tag->addProduct($small);
+
+    $entities = array_merge($entities, [
+        $image,
+        $large,
+        $medium,
+        $small,
+        $tag,
+        $attribute
+    ]);
+}
+
+function getProduct($name, $sku, $unitPrice, Image $image)
+{
+    $product = new Product();
+    $product->setSku($sku);
+    $product->setName($name);
+    $product->setUnitPrice($unitPrice);
+    $product->setIsActive(true);
+    $product->setIsVisible(true);
+    $product->setDefaultImage($image->getPath());
+
+    return $product;
 }
