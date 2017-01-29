de5f792b490c59a9615839142daa71e7d9e1c9b5
Jason Raimondi <jason@raimondi.us>
2017-01-29 13:38:41 -0800

    Remove mime_content_type function.
    We are replacing the mime_content_type function because it is returning
    incorrect responses for css pages, causing them to render incorrectly.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -888,7 +888,7 @@ class Controller extends BaseController
         }
 
         header('Content-Length: ' . filesize($filePath));
-        header('Content-Type: ' . mime_content_type($filePath));
+        header('Content-type: ' . $this->getMimeType($filePath));
         header('Expires: ' . date('r', strtotime('now +1 week')));
         header('Last-Modified: ' . date('r', filemtime($filePath)));
         header('Cache-Control: max-age=604800');
@@ -897,6 +897,40 @@ class Controller extends BaseController
 
         readfile($filePath);
         exit;
+    }
+
+    protected function getMimeType(string $file): string
+    {
+        $mime_types = [
+            "pdf" => "application/pdf",
+            "zip" => "application/zip",
+            "docx" => "application/msword",
+            "doc" => "application/msword",
+            "xls" => "application/vnd.ms-excel",
+            "ppt" => "application/vnd.ms-powerpoint",
+            "gif" => "image/gif",
+            "png" => "image/png",
+            "jpeg" => "image/jpg",
+            "jpg" => "image/jpg",
+            "mp3" => "audio/mpeg",
+            "wav" => "audio/x-wav",
+            "mpeg" => "video/mpeg",
+            "mpg" => "video/mpeg",
+            "mpe" => "video/mpeg",
+            "mov" => "video/quicktime",
+            "avi" => "video/x-msvideo",
+            "3gp" => "video/3gpp",
+            "css" => "text/css",
+            "jsc" => "application/javascript",
+            "js" => "application/javascript",
+            "php" => "text/html",
+            "htm" => "text/html",
+            "html" => "text/html",
+        ];
+
+        $extension = strtolower(end(explode('.', $file)));
+
+        return $mime_types[$extension];
     }
 
     /**
